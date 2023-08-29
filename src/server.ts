c85b6e930ed040def5cfbd80a6a3e0053af53db9
Nick Morgan <nick.morgan@coinbase.com>
2023-08-29 14:02:46 -0400

    fix api call

--- src/server.ts
+++ src/server.ts
@@ -14,20 +14,23 @@ StakingApi.getInstance();
 app.listen(PORT, async () => {
   const jwt = await StakingApi.getInstance().authenticator.buildJWT(
     "staking",
-    "https://api.developer.coinbase.com/staking/api/v1alpha1/protocols"
+    "GET api.developer.coinbase.com/staking/api/v1alpha1/protocols"
   );
   const api = new Api({
     baseApiParams: {
       headers: {
         Authorization: `Bearer ${jwt}`,
-        "Content-Type": "application/json",
       },
     },
   });
-  const output = await api.api.listProtocols().catch((e) => {
-    const httpResponse = e as HttpResponse<unknown, unknown>;
-    console.log(Object.getOwnPropertySymbols(httpResponse));
-    console.log(httpResponse.url);
-  });
+  const output = await api.api
+    .listProtocols()
+    .then((resp) => console.log(resp))
+    .catch((e) => {
+      console.log(e);
+      const httpResponse = e as HttpResponse<unknown, unknown>;
+      console.log(Object.getOwnPropertySymbols(httpResponse));
+      console.log(httpResponse.url);
+    });
   console.log(`Server is running on http://localhost:${PORT}`);
 });
